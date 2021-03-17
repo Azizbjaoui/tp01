ebce5b1a2d31548415ee9886845c5653e2bcff3f
Aziz <Aziz@PC-ACER>
2021-03-17 21:45:08 +0100

    This is my commit

---
+++ src/isetj/servlet/CoffeeServlet.java
@@ -0,0 +1,53 @@
+package isetj.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CoffeeServlet
+ */
+@WebServlet("/CoffeeServlet")
+public class CoffeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CoffeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String r = request.getParameter("r");
+		String txt = request.getParameter("txt");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>\r\n"
+				+ "<html lang=\"en\">\r\n"
+				+ "<head>");
+		out.println("<title>coffee shop</title></head>");
+		out.println("<body><img src=\"C:\\Users\\monta\\eclipse-workspace\\tp1\\WebContent\\coffe.JPG\">");
+		out.println("<p>merci de nous avoir fait parvenir la remarque suivent concernant <strong>"+r+"</strong></p>");
+		out.println();
+		out.println(txt);
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/isetj/servlet/searchservlet.java
@@ -0,0 +1,48 @@
+package isetj.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class searchservlet
+ */
+@WebServlet("/searchservlet")
+public class searchservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public searchservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String vr = request.getParameter("vr");
+		String mt = request.getParameter("mt");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>\r\n"+ "<html lang=\"en\">\r\n"+ "<head>");
+		out.println("<a href=\"http://https://www.google.com/\">google</a>");
+		
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
